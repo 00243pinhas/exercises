64010c755e9dc04fd9bdd1b652d7e0c9f14a9ff5
00243pinhas <pinaslo@gmail.com>
2023-01-09 13:59:36 +0300

    #flipp photos

--- index.js
+++ index.js
@@ -1,61 +1,134 @@
+var reviews = [
+  {
+    id: 1,
+    name: "susan smith",
+    job: "web developer",
+    img:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+    text:
+      "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+  },
 
-const value=document.querySelector('#value');
 
-const btn1=document.querySelectorAll(".btn");
+// first objext
+  {
+    id: 2,
+    name: "anna johnson",
+    job: "web designer",
+    img:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+    text:
+      "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
+  },
 
-let count = 0;
+// second object
+
+  {
+    id: 3,
+    name: "peter jones",
+    job: "intern",
+    img:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+    text:
+      "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
+  },
 
 
-btn1.forEach(function(item){
-  // The forEach() method calls a function for each element in an array.
-  // itmes method is actually one btn
-  item.addEventListener('click', function(e){
-    // e is showing u where is the is the curent click 
-    const style=(e.currentTarget.classList);
+  // third object
+  {
+    id: 4,
+    name: "bill anderson",
+    job: "the boss",
+    img:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+    text:
+      "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
+  },
+];
 
-    if(style.contains('decrease')){
-      count--;
-      value.textContent=count;
-    }
-    else if (style.contains('increase')){
-      count++;
-      value.textContent=count;
-    }
 
-    else{
-      count=0;
-      value.textContent=count;
-    }
+//console.log(reviews.length)
 
-    if (count>0){
-      value.style.color='Green';
-    }
-    if (count<0){
-      value.style.color='red';
-    }
 
-  });
+// select itmes
+
+const img=document.getElementById('person-img');
+
+const auther=document.getElementById('author');
+
+const job = document.getElementById('job');
+
+const info= document.getElementById('info');
+
+
+const prevBtn= document .querySelector('.prev-btn');
+
+const nextBtn= document. querySelector('.next-btn');
+
+const randomBtn= document.querySelector('.random-btn');
+
+let curentItem=0;
+
+
+window.addEventListener('DOMContentLoaded',function(){
+    ShowTheperson(curentItem)
 });
 
 
+// this function is holding all the info and the swich 
+function ShowTheperson(person){
+
+  const item= reviews[person];
+  img.src=item.img;
+  auther.textContent=item.name;
+  job.textContent=item.job;
+  info.textContent=item.text;
+
+};
+
+// swich next 
+
+nextBtn.addEventListener('click',function(){
+
+  curentItem++;
+
+  if (curentItem>reviews.length-1){
+    curentItem=0
+    ShowTheperson(curentItem)
+  }else{
+    ShowTheperson(curentItem);
+  }
+   
+});
+
+prevBtn.addEventListener('click',function (){
+
+  if (curentItem<0){
+    curentItem=reviews.length-1
+    ShowTheperson(curentItem);
+  }else{
+    curentItem--;
+    ShowTheperson(curentItem)
+  }
+
+});
+
+// random choice ...
+randomBtn.addEventListener('click', function(){
+  curentItem=randomChoice()
+
+  ShowTheperson(curentItem);
+});
+
+function randomChoice(){
+
+  choice= Math.floor(Math.random()*reviews.length-1)
+  return choice;
+}
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ 
 
 
 
