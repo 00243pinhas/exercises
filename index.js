8c7af5a2bad8647f61feb67992b4252a04e9d265
00243pinhas <pinaslo@gmail.com>
2023-01-10 18:58:41 +0300

    # side bare

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
+const toggle = document.querySelector('.sidebar-toggle');
 
+const closBarre= document.querySelector('.close-btn');
 
-const toggle=document.querySelector('.nav-toggle');
-
-const links=document.querySelector('.links');
+const sidebar= document.querySelector('.sidebar');
 
 
 toggle.addEventListener('click',function(){
-  //console.log(links.classList.contains('links'))
-
-  // if (links.classList.contains('show-links')){
-  //     links.classList.remove('show-links')
-  // }else{
-  //   links.classList.add('show-links')
-  // }
-  // or we can only use the toggle methode specifyinh the css class we wanna toggle 
-
-  links.classList.toggle('show-links');
+ 
+  if (sidebar.classList.contains('show-sidebar')){
+    sidebar.classList.remove('show-sidebar')
+  }else{
+    sidebar.classList.add('show-sidebar')
+  }
 });
+
+
+closBarre.addEventListener('click',function(){
+  sidebar.classList.remove('show-sidebar')
+})
