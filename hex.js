05c0d22a3edc8d5b6a5394b9b10d940462b4efec
00243pinhas <pinaslo@gmail.com>
2023-01-10 17:39:16 +0300

    # nav barre

--- hex.js
+++ hex.js
@@ -1,131 +1,23 @@
-var reviews = [
-    {
-      id: 1,
-      name: "susan smith",
-      job: "web developer",
-      img:
-        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
-      text:
-        "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
-    },
-  
-  
-  // first objext
-    {
-      id: 2,
-      name: "anna johnson",
-      job: "web designer",
-      img:
-        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
-      text:
-        "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
-    },
-  
-  // second object
-  
-    {
-      id: 3,
-      name: "peter jones",
-      job: "intern",
-      img:
-        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
-      text:
-        "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
-    },
-  
-  
-    // third object
-    {
-      id: 4,
-      name: "bill anderson",
-      job: "the boss",
-      img:
-        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
-      text:
-        "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
-    },
-  ];
-
-  
-const img=document.getElementById('person-img');
-
-const author=document.getElementById('author');
-
-const job = document.getElementById('job');
-
-const text = document.getElementById('info');
 
 
-const prevBtn=document.querySelector('.prev-btn');
+const toggle = document.querySelector('.nav-toggle');
 
-const nextBtn = document.querySelector('.next-btn');
-
-const randomBtn= document.querySelector('.random-btn');
+const links =document.querySelector('.links');
 
 
-// here is our counter variable
+toggle.addEventListener('click', function(){
+   //console.log('hey');
 
+   //if (links.classList.contains('show-links')){
+    // in the links class list we are removing the show-links class
+//         links.classList.remove('show-links')
+//    }else{
 
-let countPersons=1;
+//     links.classList.add('show-links')
+//    }
 
+// in the shortest way we use the toggle methode 
+// this one will do the operation 
 
-
-
-//windows.addEventlisterner is used cuz we need to see the even right  away we open the windows
-
-window.addEventListener('DOMContentLoaded',function(){
-    
-    // revievs is ur object
-    //[counterPesons]=> this is indicating the element in the revievs object 
-    automatics(countPersons);
-});
-
-  
-function automatics(persons){
-    
-    const item= reviews[persons];
-    
-    img.src=item.img;
-    author.textContent=item.name;
-    job.textContent=item.job;
-    text.textContent=item.text;
-};
-
-prevBtn.addEventListener('click',function(){
-
-    if (curentItem>0){
-        curentItem=reviews.length-1
-        automatics(countPersons);
-    }else{
-        countPersons--;
-        automatics(countPersons);
-
-    }
-});
-
-
-nextBtn.addEventListener('click',function(){
-    
-    if (countPersons>countPersons.length-1){
-        
-        curentItem=0 
-        automatics(curentItem)
-    }else{
-        countPersons++;
-        automatics(countPersons)
-    }
-});
-
-
-randomBtn.addEventListener('click',function(){
-    shoose=randomChoice()
-    automatics(shoose);
-});
-
-
-function randomChoice(){
-
-    choice=Math.floor(Math.random()*reviews.length-1);
-    return choice;
-}
-
+links.classList.toggle('show-links')
+})
--- index.js
+++ index.js
@@ -1,138 +1,19 @@
-var reviews = [
-  {
-    id: 1,
-    name: "susan smith",
-    job: "web developer",
-    img:
-      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
-    text:
-      "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
-  },
 
 
-// first objext
-  {
-    id: 2,
-    name: "anna johnson",
-    job: "web designer",
-    img:
-      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
-    text:
-      "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
-  },
+const toggle=document.querySelector('.nav-toggle');
 
-// second object
-
-  {
-    id: 3,
-    name: "peter jones",
-    job: "intern",
-    img:
-      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
-    text:
-      "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
-  },
+const links=document.querySelector('.links');
 
 
-  // third object
-  {
-    id: 4,
-    name: "bill anderson",
-    job: "the boss",
-    img:
-      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
-    text:
-      "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
-  },
-];
+toggle.addEventListener('click',function(){
+  //console.log(links.classList.contains('links'))
 
+  // if (links.classList.contains('show-links')){
+  //     links.classList.remove('show-links')
+  // }else{
+  //   links.classList.add('show-links')
+  // }
+  // or we can only use the toggle methode specifyinh the css class we wanna toggle 
 
-//console.log(reviews.length)
-
-
-// select itmes
-
-const img=document.getElementById('person-img');
-
-const auther=document.getElementById('author');
-
-const job = document.getElementById('job');
-
-const info= document.getElementById('info');
-
-
-const prevBtn= document .querySelector('.prev-btn');
-
-const nextBtn= document. querySelector('.next-btn');
-
-const randomBtn= document.querySelector('.random-btn');
-
-let curentItem=0;
-
-
-window.addEventListener('DOMContentLoaded',function(){
-    ShowTheperson(curentItem)
+  links.classList.toggle('show-links');
 });
-
-
-// this function is holding all the info and the swich 
-function ShowTheperson(person){
-
-  const item= reviews[person];
-  img.src=item.img;
-  auther.textContent=item.name;
-  job.textContent=item.job;
-  info.textContent=item.text;
-
-};
-
-// swich next 
-
-nextBtn.addEventListener('click',function(){
-
-  curentItem++;
-
-  if (curentItem>reviews.length-1){
-    curentItem=0
-    ShowTheperson(curentItem)
-  }else{
-    ShowTheperson(curentItem);
-  }
-   
-});
-
-prevBtn.addEventListener('click',function (){
-
-  if (curentItem<0){
-    curentItem=reviews.length-1
-    ShowTheperson(curentItem);
-  }else{
-    curentItem--;
-    ShowTheperson(curentItem)
-  }
-
-});
-
-// random choice ...
-randomBtn.addEventListener('click', function(){
-  curentItem=randomChoice()
-
-  ShowTheperson(curentItem);
-});
-
-function randomChoice(){
-
-  choice= Math.floor(Math.random()*reviews.length-1)
-  return choice;
-}
-
-
-
- 
-
-
-
-
-
-
-
